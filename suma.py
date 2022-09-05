8be1f71471b0b55ed2b2671bbdbaa7e8ee80b5bc
KVSUMANTH123 <112843763+KVSUMANTH123@users.noreply.github.com>
2022-09-05 10:25:16 +0530

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,1 +1,6 @@
-import pandas as pd
+import streamlit as st
+st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
+a=st.number_input('enter any value')
+if st.button('HIT ME'):
+  st.success(f'your lucky number is (a)')
+  
