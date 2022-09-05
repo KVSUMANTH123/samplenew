406058998f806c7791d92b5c430a6e4674df6347
KVSUMANTH123 <112843763+KVSUMANTH123@users.noreply.github.com>
2022-09-05 10:26:38 +0530

    Update suma.py

--- suma.py
+++ suma.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
 a=st.number_input('enter any value')
 if st.button('HIT ME'):
-  st.success(f'your lucky number is (a)')
+  st.success(f'your lucky number is {a}')
   
